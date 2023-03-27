9b42578ee0a7329db3cd33782734ffd1e41900a9
VirxEC <virx@virxcase.dev>
2023-03-27 17:41:17 -0400

    Lock cursor

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod rocketsim;
+
 use rocketsim::RocketSimPlugin;
 
 use bevy::{
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
-    prelude::*,
+    prelude::*, window::{CursorGrabMode, PrimaryWindow},
 };
 use bevy_atmosphere::prelude::*;
 use smooth_bevy_cameras::{
@@ -21,13 +22,39 @@ fn main() {
         .add_plugin(LookTransformPlugin)
         .add_plugin(FpsCameraPlugin::default())
         .add_startup_system(setup)
+        .add_system(grab_mouse)
         .run();
+}
+
+fn grab_mouse(
+    mut windows: Query<&mut Window, With<PrimaryWindow>>,
+    mouse: Res<Input<MouseButton>>,
+    key: Res<Input<KeyCode>>,
+) {
+    let mut window = windows.single_mut();
+    if mouse.just_pressed(MouseButton::Left) {
+        window.cursor.visible = false;
+        window.cursor.grab_mode = if cfg!(windows) {
+            CursorGrabMode::Confined
+        } else {
+            CursorGrabMode::Locked
+        };
+    }
+    if key.just_pressed(KeyCode::Escape) {
+        window.cursor.visible = true;
+        window.cursor.grab_mode = CursorGrabMode::None;
+    }
 }
 
 fn setup(mut commands: Commands) {
     commands
         .spawn(FpsCameraBundle::new(
-            FpsCameraController::default(),
+            FpsCameraController {
+                enabled: true,
+                mouse_rotate_sensitivity: Vec2::splat(0.75),
+                translate_sensitivity: 2.0,
+                smoothing_weight: 0.9,
+            },
             Vec3::default(),
             Vec3::X,
             Vec3::Y,
