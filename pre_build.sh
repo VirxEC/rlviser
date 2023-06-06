146e7cf539657e38a1db3ae744626734f40e6e99
Eric Veilleux <virx@virxcase.dev>
2023-06-05 21:55:45 -0400

    Update pre_build.sh

--- pre_build.sh
+++ pre_build.sh
@@ -1,1 +1,1 @@
-sudo apt install g++ pkg-config libx11-dev libasound2-dev libudev-dev libwayland-dev libxkbcommon-dev
+apt install g++ pkg-config libx11-dev libasound2-dev libudev-dev libwayland-dev libxkbcommon-dev
