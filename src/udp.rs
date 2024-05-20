b4595845da198ed139a478c331a6df302b63e0bc
VirxEC <virx@virxcase.dev>
2024-05-19 20:04:15 -0400

    Fix windows compile error

--- src/udp.rs
+++ src/udp.rs
@@ -489,7 +489,7 @@ fn start_udp_recv_handler(socket: UdpSocket, commands: &mut Commands) {
 
                     #[cfg(windows)]
                     {
-                        while let Err(e) = socket.0.peek_from(&mut initial_state_buffer) {
+                        while let Err(e) = socket.peek_from(&mut initial_state_buffer) {
                             if let Some(code) = e.raw_os_error() {
                                 if code == 10040 {
                                     break;
@@ -522,7 +522,7 @@ fn start_udp_recv_handler(socket: UdpSocket, commands: &mut Commands) {
                 UdpPacketTypes::Render => {
                     #[cfg(windows)]
                     {
-                        while let Err(e) = socket.0.peek_from(&mut initial_state_buffer) {
+                        while let Err(e) = socket.peek_from(&mut initial_state_buffer) {
                             if let Some(code) = e.raw_os_error() {
                                 if code == 10040 {
                                     break;
